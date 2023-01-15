7c7e744daaf4a259378604c93bba7ece108cb92a
PythonMonty <91922582+Python125@users.noreply.github.com>
2023-01-15 17:29:16 -0500

    Update home.js

--- Home Page/home.js
+++ Home Page/home.js
@@ -1,15 +1,15 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 let quotes = [
-    'I can see you right now in the kitchen, bending over a hot stove. But I can`t see the stove. - Rufus T. Firefly (Duck Soup, 1933)',
-    'Of all the gin joints, in all the towns, in all the world, she walks into mine. - Rick (Casablanca, 1942)',
-    'Here`s looking at you, kid. - Rick (Casablanca, 1942)',
-    'Fasten your seat belts, it`s going to be a bumpy night! - Margo (All About Eve, 1950)',
-    'Frankly, my dear, I don`t give a damn. - Rhett Butler (Gone with the Wind, 1939)',
-    'Public enemy, he calls me! - Johnny Rocco (Key Largo, 1948)',
-    'Mother of Mercy! Is this the end of Rico? - Caesar Enrico Bandello (Little Caesar, 1931)',
-    'To be afraid, you gotta have a heart. I don`t think I got one. I had that cut out of me a long time ago. - Rocky Sullivan (Angels with Dirty Faces, 1938)',
-    'My great aunt Jennifer ate a whole box of candy every day of her life. She lived to be 102 and when she`d been dead three days she looked better than you do *now!* - Sheridan Whiteside (The Man Who Came to Dinner, 1942)'
+    '"I can see you right now in the kitchen, bending over a hot stove. But I can`t see the stove." <br><br> Rufus T. Firefly (Duck Soup, 1933)',
+    '"Of all the gin joints, in all the towns, in all the world, she walks into mine." <br><br> Rick (Casablanca, 1942)',
+    '"Here`s looking at you, kid." <br><br> Rick (Casablanca, 1942)',
+    '"Fasten your seat belts, it`s going to be a bumpy night!" <br><br> Margo (All About Eve, 1950)',
+    '"Frankly, my dear, I don`t give a damn." <br><br> Rhett Butler (Gone with the Wind, 1939)',
+    '"Public enemy, he calls me!" <br><br> Johnny Rocco (Key Largo, 1948)',
+    '"Mother of Mercy! Is this the end of Rico?" <br><br> Caesar Enrico Bandello (Little Caesar, 1931)',
+    '"To be afraid, you gotta have a heart. I don`t think I got one. I had that cut out of me a long time ago." <br><br> Rocky Sullivan (Angels with Dirty Faces, 1938)',
+    '"My great aunt Jennifer ate a whole box of candy every day of her life. She lived to be 102 and when she`d been dead three days she looked better than you do *now!*" <br><br> Sheridan Whiteside (The Man Who Came to Dinner, 1942)',
   ];
 
 btn.addEventListener('click', function(){
@@ -205,3 +205,43 @@ const questions = [
     ]
   },
 ]
+
+const previous = document.getElementsByClassName("previous")[0];
+const next = document.getElementsByClassName("next")[0];
+const image_div = document.querySelector(".images");
+let left = parseFloat(window.getComputedStyle(image_div).getPropertyValue("left"));
+let images = image_div.querySelectorAll("img");
+const image_width = parseFloat(window.getComputedStyle(images[0]).getPropertyValue("width"));
+const image_margin_right = parseFloat(window.getComputedStyle(images[0]).getPropertyValue("margin-right"));
+
+
+function image_copy_add(){
+	for (let i = 0 ; i<= 20 ; i++){
+		images.forEach(function(item){
+            let img = document.createElement("img");
+            let a_tag = document.createElement("a");
+            a_tag.href = "#";
+            img.src = item.src;
+            a_tag.appendChild(img);
+			image_div.appendChild(a_tag);
+		});
+	}
+}
+image_copy_add();
+function previous_function(){
+	if (left < 0) {
+		left = left + (image_width + image_margin_right);
+		image_div.style.left = left + "px";
+	}
+}
+function next_function(){
+	images = image_div.querySelectorAll("img");
+	left = (left-(image_width + image_margin_right));
+	image_div.style.left = parseFloat(left) + "px";
+}
+previous.addEventListener("click",() => {
+	previous_function();
+});
+next.addEventListener("click",() => {
+	next_function();
+});
