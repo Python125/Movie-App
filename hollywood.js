983da7a269224081879ff48beb5421f0d8d84190
PythonMonty <91922582+Python125@users.noreply.github.com>
2023-01-30 14:29:24 -0500

    Update hollywood.js

--- hollywood.js
+++ hollywood.js
@@ -264,6 +264,8 @@ next.addEventListener("click",() => {
   next_function();
 });
 
+
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
