74e2589da570b154018257fb85d646989b76cbd4
PythonMonty <91922582+Python125@users.noreply.github.com>
2023-02-02 10:17:05 -0500

    Update hollywood.js

--- hollywood.js
+++ hollywood.js
@@ -18,7 +18,6 @@ let quotes = [
     '"<b><i>My great aunt Jennifer ate a whole box of candy every day of her life. She lived to be 102 and when she`d been dead three days she looked better than you do *now!*</i></b>" <br><br> Sheridan Whiteside - played by <u>Monty Woolley</u> <br><br> (<i>The Man Who Came to Dinner</i>, <b>1942</b>)',
     '"<b><i>Public enemy, he calls me!</i></b>" <br><br> Johnny Rocco - played by <u>Edward G. Robinson</u> <br><br> (<i>Key Largo</i>, <b>1948</b>)',
     '"<b><i>Made it, Ma! Top of the world!</i></b>" <br><br> Cody Jarrett - played by <u>James Cagney</u> <br><br> (<i>White Heat</i>, <b>1949</b>)',
-    '"<b><i>Fasten your seat belts, it`s going to be a bumpy night!</i></b>" <br><br> Margo - played by <u>Bette Davis</u> <br><br> (<i>All About Eve</i>, <b>1950</b>)',
 ];
 
 btn.addEventListener('click', function(){
