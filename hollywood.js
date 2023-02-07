58f2d6ac6fbe07e4b85be3e5c33e3fa142e24fd7
PythonMonty <91922582+Python125@users.noreply.github.com>
2023-02-07 13:37:41 -0500

    Update hollywood.js

--- hollywood.js
+++ hollywood.js
@@ -1,3 +1,74 @@
+const sliderContainers = document.querySelectorAll(".container");
+sliderContainers.forEach((container) => {
+  const images = container.querySelectorAll(".images img");
+  const prevButton = container.querySelector(".buttons .previous");
+  const nextButton = container.querySelector(".buttons .next");
+  let currentImageIndex = 0;
+
+  const showImage = (imageIndex) => {
+    images.forEach((img, index) => {
+      if (index === imageIndex) {
+        img.style.display = "block";
+      } else {
+        img.style.display = "none";
+      }
+    });
+  };
+
+  prevButton.addEventListener("click", () => {
+    currentImageIndex--;
+    if (currentImageIndex < 0) {
+      currentImageIndex = images.length - 1;
+    }
+    showImage(currentImageIndex);
+  });
+
+  nextButton.addEventListener("click", () => {
+    currentImageIndex++;
+    if (currentImageIndex === images.length) {
+      currentImageIndex = 0;
+    }
+    showImage(currentImageIndex);
+  });
+});
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
 let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 let quotes = [
@@ -214,77 +285,6 @@ const questions = [
     ]
   },
 ]
-
-const sliderContainers = document.querySelectorAll(".container");
-sliderContainers.forEach((container) => {
-  const images = container.querySelectorAll(".images img");
-  const prevButton = container.querySelector(".buttons .previous");
-  const nextButton = container.querySelector(".buttons .next");
-  let currentImageIndex = 0;
-
-  const showImage = (imageIndex) => {
-    images.forEach((img, index) => {
-      if (index === imageIndex) {
-        img.style.display = "block";
-      } else {
-        img.style.display = "none";
-      }
-    });
-  };
-
-  prevButton.addEventListener("click", () => {
-    currentImageIndex--;
-    if (currentImageIndex < 0) {
-      currentImageIndex = images.length - 1;
-    }
-    showImage(currentImageIndex);
-  });
-
-  nextButton.addEventListener("click", () => {
-    currentImageIndex++;
-    if (currentImageIndex === images.length) {
-      currentImageIndex = 0;
-    }
-    showImage(currentImageIndex);
-  });
-});
-
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
 
 function submitComment() {
   const comment = document.getElementById("comment").value;
