3eba0b506df224d1094907f935b3821d28489385
PythonMonty <91922582+Python125@users.noreply.github.com>
2023-02-10 10:44:29 -0500

    Update spare.js

--- spare.js
+++ spare.js
@@ -1,10 +1,4 @@
-
-// This function is called when the user submits a comment
-function submitComment() {
-  const comment = document.getElementById("comment").value;
-  const commentsDiv = document.getElementById("comments");
-
-  // Make a request using the Axios library
+// Make a request using the Axios library
   axios.post('/send-email', {
     comment: comment
   })
@@ -14,86 +8,6 @@ function submitComment() {
   .catch(function (error) {
     console.log(error);
   });
-
-  const newCommentContainer = document.createElement("div");
-
-  const newComment = document.createElement("div");
-  newComment.innerHTML = comment;
-  newCommentContainer.appendChild(newComment);
-
-  const dateTime = new Date();
-  const formattedDateTime = dateTime.toLocaleString();
-  const dateTimeElement = document.createElement("div");
-  dateTimeElement.innerHTML = formattedDateTime;
-  newCommentContainer.appendChild(dateTimeElement);
-
-  const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "Delete";
-  deleteButton.classList.add("delete-button");
-  deleteButton.onclick = function() {
-    commentsDiv.removeChild(newCommentContainer);
-
-    // Remove the comment from local storage
-    const existingComments = JSON.parse(localStorage.getItem("comments")) || [];
-    const index = existingComments.indexOf(comment);
-    if (index !== -1) {
-      existingComments.splice(index, 1);
-      localStorage.setItem("comments", JSON.stringify(existingComments));
-    }
-  };
-  newCommentContainer.appendChild(deleteButton);
-
-  commentsDiv.appendChild(newCommentContainer);
-
-  // Save the comment and its date/time to local storage
-  const existingComments = JSON.parse(localStorage.getItem("comments")) || [];
-  existingComments.push({ comment, dateTime: formattedDateTime });
-  localStorage.setItem("comments", JSON.stringify(existingComments));
-
-  document.getElementById("comment").value = "";
-}
-
-// This function is called when the page loads
-window.onload = function() {
-  const comments = JSON.parse(localStorage.getItem("comments")) || [];
-  const commentsDiv = document.getElementById("comments");
-
-  comments.forEach(function(commentObject) {
-    const newCommentContainer = document.createElement("div");
-
-    const newComment = document.createElement("div");
-    newComment.innerHTML = commentObject.comment;
-    newCommentContainer.appendChild(newComment);
-
-    const dateTimeElement = document.createElement("div");
-    dateTimeElement.innerHTML = commentObject.dateTime;
-    newCommentContainer.appendChild(dateTimeElement);
-
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    deleteButton.classList.add("delete-button");
-    deleteButton.onclick = function() {
-      commentsDiv.removeChild(newCommentContainer);
-
-// Remove the comment from local storage
-      const existingComments = JSON.parse(localStorage.getItem("comments")) || [];
-      const index = existingComments.indexOf(commentObject);
-      if (index !== -1) {
-        existingComments.splice(index, 1);
-        localStorage.setItem("comments", JSON.stringify(existingComments));
-      }
-    };
-    newCommentContainer.appendChild(deleteButton);
-
-    commentsDiv.appendChild(newCommentContainer);
-  });
-}
-
-
-
-
-
-
 
 const express = require('express');
 const expressApp = express();
